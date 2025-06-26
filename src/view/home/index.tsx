2b6ba06175ec7162b2935491b511a9f923bcad64
restuak <restuaka.works@gmail.com>
2025-06-26 12:09:30 +0700

    feat: making components

--- src/view/home/index.tsx
+++ src/view/home/index.tsx
@@ -1,17 +1,36 @@
-import Image from "next/image";
+"use client";
+import React, { useEffect, useState } from "react";
+import Aos from "aos";
+import "aos/dist/aos.css";
+import Loading from "../../components/loading";
 export default function HomeView() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 2800);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  useEffect(() => {
+    Aos.init({
+      offset: 100,
+      once: true,
+    });
+  });
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-      </main>
-    </div>
+    <main className="">
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <div className="" id="home">
+
+       
+        </div>
+      )}
+    </main>
   );
 }
