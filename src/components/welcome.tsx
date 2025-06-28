2bc503c58e4acfc87de9ac8205101a596bfe2a10
restuak <restuaka.works@gmail.com>
2025-06-28 16:18:23 +0700

    feat: animated

--- src/components/welcome.tsx
+++ src/components/welcome.tsx
@@ -47,7 +47,7 @@ export default function Welcome() {
 
         <div className="absolute bottom-0 left-0 w-full flex justify-center pb-10 overflow-y-hidden">
           <div className="animate-fade-up animate-once animate-delay-2000 animate-ease-out z-20 flex flex-col items-center cursor-pointer">
-            <span className="animate-bounce text-4xl text-bold ">
+            <span className="animate-bounce text-4xl text-bold text-white">
               <RiArrowDownWideFill />
             </span>
           </div>
