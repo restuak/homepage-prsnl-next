db80aa78fbb60cc15867951e2ccff52d0675c4e7
restuak <restuaka.works@gmail.com>
2025-07-03 03:00:56 +0700

    feat: delete error

--- src/app/works/layout.tsx
+++ src/app/works/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 export const metadata: Metadata = {
-  title: "Experience Restu Aka",
-  description: "Page experience Restu Aka, a fullstack developer",
+  title: "Experience Restu Aka | Restu Aka's Personal Website",
+  description:
+    "Page experience and skills Restu Aka, a fullstack developer, geodet, geomatics, geoinformatics,",
 };
 
 export default function Layout({
