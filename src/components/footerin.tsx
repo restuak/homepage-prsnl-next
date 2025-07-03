ec97a833e0b9a8eba8f4cdacf8b8e3b8355c7d4a
restuak <restuaka.works@gmail.com>
2025-07-03 14:38:13 +0700

    feat: edit content

--- src/components/footerin.tsx
+++ src/components/footerin.tsx
@@ -11,16 +11,16 @@ export default function FooterIn() {
         className="flex flex-row justify-center items-center "
       >
         <div className="max-w-md">
-          <h4 className="border-t-3 border-zinc-600 mb-0 text-4xl text-zinc-300">
-            LET'S CONNECT
+          <h4 className="border-t-3 border-zinc-600 mb-0 text-xl md:text-3xl text-zinc-300">
+            WANNA SAY SOMETHING?
           </h4>
           <a
             href="/#contact"
             rel="noopener noreferrer"
             aria-label="Get In Touch"
-            className="p-3 flex justify-center items-center text-lg font-bold bg-zinc-300 w-full h-full rounded-b-2xl text-zinc-600 hover:text-zinc-200 hover:bg-zinc-600 transition-all ease-in-out duration-300 "
+            className="p-3 flex justify-center items-center text-sm md:text-lg font-bold bg-zinc-300 w-full h-full rounded-b-2xl text-zinc-600 hover:text-zinc-200 hover:bg-zinc-600 transition-all ease-in-out duration-300 "
           >
-            GET IN TOUCH HERE, GUYS!
+            FREE TO CONNECT, GUYS!
           </a>
         </div>
       </motion.div>
